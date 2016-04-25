c22fb6bcd70fcb97515ff1cd1a837c697b074424
Dustin Hurst <the_friendship_warrior@hotmail.com>
2016-04-25 13:41:05 -0600

    Made external files.
    Put js into its own file instead of embedded into the index file. Also
    mongo and sql have their own files.

--- siq.js
+++ siq.js
@@ -1,28 +1,16 @@
 /**
- * Dustin Hurst
- * Siq App
- * created 2/28/2016
+ * Created by Dustin on 2/23/16.
  */
-console.log('Loading Server');
+console.log('Loading...');
 var fs = require('fs');
 var express = require('express');
+var mongoDao = require('./mongoDao');
 
 //modules below are express middleware
 var bodyParser = require('body-parser');
 var logger = require('morgan');
 var compression = require('compression');
 var favicon = require('serve-favicon');
-var _ = require('underscore');
-
-var mysql = require('mysql');
-var connection = mysql.createConnection({
-    host : 'localhost',
-    user : 'root',
-    password : 'root',
-    database : 'siq'
-});
-
-connection.connect();
 
 var app = express();
 
@@ -42,75 +30,13 @@ app.use(compression());
 
 app.use(allowCrossDomain);
 
-var db = [
-    { id:1, "panelHeading":"Foo", "panelBody":"All your base are belong to us!" },
-    { id:2, "panelHeading":"Bar", "panelBody":"I can haz cheesburger?" },
-    { id:3, "panelHeading":"Baz", "panelBody":"Hooked on phonics worked for me!" }
-];
+app.use('/', mongoDao);
 
-//REST API calls go here.
-app.get('/api/testdb', function(req, res){
-    connection.query("select * from entries", function(err, rows, fields) {
-        if (err) throw err;
-//console.log('The solution is: ', rows[0].solution);
-        res.json(rows);
-    });
-});
-
-app.get('/api/entries', function(req, res) {
-    connection.query("select subject, id from entries", function(err, rows, fields) {
-        if (err) throw err;
-//console.log('The solution is: ', rows[0].solution);
-        res.json(rows);
-    });
-});
-
-// IDEMPOTENT - You can repeat the operation as many times as you want without changing state.
-// Create
-app.post('/api/entries', function(req, res){
-// Store new entry and return id.
-    console.log(req.body);
-// {"subject":"Two","content":"content2"}
-    var subject = req.body.subject;
-    var content = req.body.content;
-
-// TODO escape query for SQL injection
-    connection.query(`INSERT INTO entries (subject, content) VALUES( '${subject}', '${content}')`, function(err, rows, fields) {
-        if (err) throw err;
-        res.json(rows.insertId);
-    });
-});
-
-// Read
-app.get('/api/entries/:id', function(req, res){
-    var id = connection.escape(req.params.id);
-//var id = req.params.id;
-    console.log(`select * from entries where id = ${id}`);
-    connection.query(`select * from entries where id = ${id}`, function(err, rows, fields) {
-        if (err) throw err;
-        res.json(rows[0]);
-    });
-});
-
-// Update
-app.put('/api/entries/:id', function(req, res){
-
-});
-
-// Delete
-app.delete('/api/entries/:id', function(req, res){
-    var id = connection.escape(req.params.id);
-    connection.query(`delete from entries where id = ${id}`, function(err, rows, fields) {
-        if (err) throw err;
-    });
-    console.log("server delete ");
-    res.sendStatus(200);
-});
 
 //traditional webserver stuff for serving static files
 var WEB = __dirname + '/web';
 app.use(favicon(WEB + '/favicon.ico'));
-app.use(express.static(WEB));
+app.use(express.static(WEB, {maxAge: '12h'}));
 app.get('*', function(req, res) {
     res.header('Access-Control-Allow-Origin', '*');
     res.status(404).sendFile(WEB + '/404Error.png');
@@ -120,22 +46,21 @@ app.get('*', function(req, res) {
 var port = process.env.port || 3000;
 var server = app.listen(port);
 
-function gracefullShutdown(){
-    console.log('\nStarting Shutdown');
+function gracefulShutdown(){
+    console.log("\nStarting shutdown...");
     server.close(function(){
-        console.log("before end call");
         connection.end();
-        console.log('\nShutdown Complete');
+        console.log('Shutdown complete.');
     });
 }
 
 process.on('SIGTERM', function(){
-    gracefullShutdown();
+    gracefulShutdown();
 });
 
 process.on('SIGINT', function(){
-    gracefullShutdown();
+    gracefulShutdown();
 });
 
-console.log(`Listening on port ${port}`);
 
+console.log(`Listening on port ${port}`);
